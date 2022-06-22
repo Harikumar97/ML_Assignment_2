217d48b9377f85a8578ef7bd2438c3d12bc24274
Harikumar97 <hrkmrv@gmail.com>
2022-06-22 21:36:05 +0400

    first commit

---
+++ ANN_SurfaceCrack.jl
@@ -0,0 +1,120 @@
+### A Pluto.jl notebook ###
+# v0.19.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 5172ea02-f225-11ec-10aa-2721af1393d9
+
+
+# ╔═╡ 1997623b-948f-4fd1-b7b6-8bfefd38f042
+
+
+# ╔═╡ e4603ba9-b2f6-40ae-946b-7c49d292a040
+
+
+# ╔═╡ a266e013-eb1c-43d7-bacc-17064fb0b758
+
+
+# ╔═╡ b1082bf9-02b4-46ce-96ae-caa0ca6cffd4
+
+
+# ╔═╡ 470efa89-e261-4696-ae6d-51e554505542
+
+
+# ╔═╡ 89e97e91-d725-4f18-87a3-6f2bddcd488f
+
+
+# ╔═╡ 1e66e5b7-5bd6-4c94-a275-7a704b163e31
+
+
+# ╔═╡ ad4eb2af-0671-4d45-82c2-08b89d870d1f
+
+
+# ╔═╡ cb0fb067-38d9-4408-ac72-7c3d345b2c51
+
+
+# ╔═╡ c18d277b-7a8b-4b15-9b50-9ca40e100b1e
+
+
+# ╔═╡ 9ad44c18-ec33-470e-b4a2-803edf469792
+
+
+# ╔═╡ 81f1d01f-a29d-446b-b124-c6c992e9d465
+
+
+# ╔═╡ 1d20e731-5ebc-4f69-b57a-8fc12d78e520
+
+
+# ╔═╡ e309a5ad-7788-4b8e-b3d6-78e69e1f46c1
+
+
+# ╔═╡ de6c68b2-aca3-45ea-bd9a-58165788b173
+
+
+# ╔═╡ 833bb1ad-8f76-4652-8e52-c64b70b8c052
+
+
+# ╔═╡ ba8992c8-9de1-43ff-85a4-7e02830de4a7
+
+
+# ╔═╡ 6b3e0465-d1e0-4bd2-9522-bb22786c7be2
+
+
+# ╔═╡ 08c48358-fb42-4da5-a873-4f5ee13de9d2
+
+
+# ╔═╡ 6a03eb1b-733c-4a9c-9ffb-73ebfbf068ac
+
+
+# ╔═╡ 2db193b4-0165-4c23-9ebe-10b8b4ea086d
+
+
+# ╔═╡ 84c5e859-54fb-422b-8c6c-71297d687c52
+
+
+# ╔═╡ a46a2cc1-74be-4a85-a537-35afdc772aa9
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═5172ea02-f225-11ec-10aa-2721af1393d9
+# ╠═1997623b-948f-4fd1-b7b6-8bfefd38f042
+# ╠═e4603ba9-b2f6-40ae-946b-7c49d292a040
+# ╠═a266e013-eb1c-43d7-bacc-17064fb0b758
+# ╠═b1082bf9-02b4-46ce-96ae-caa0ca6cffd4
+# ╠═470efa89-e261-4696-ae6d-51e554505542
+# ╠═89e97e91-d725-4f18-87a3-6f2bddcd488f
+# ╠═1e66e5b7-5bd6-4c94-a275-7a704b163e31
+# ╠═ad4eb2af-0671-4d45-82c2-08b89d870d1f
+# ╠═cb0fb067-38d9-4408-ac72-7c3d345b2c51
+# ╠═c18d277b-7a8b-4b15-9b50-9ca40e100b1e
+# ╠═9ad44c18-ec33-470e-b4a2-803edf469792
+# ╠═81f1d01f-a29d-446b-b124-c6c992e9d465
+# ╠═1d20e731-5ebc-4f69-b57a-8fc12d78e520
+# ╠═e309a5ad-7788-4b8e-b3d6-78e69e1f46c1
+# ╠═de6c68b2-aca3-45ea-bd9a-58165788b173
+# ╠═833bb1ad-8f76-4652-8e52-c64b70b8c052
+# ╠═ba8992c8-9de1-43ff-85a4-7e02830de4a7
+# ╠═6b3e0465-d1e0-4bd2-9522-bb22786c7be2
+# ╠═08c48358-fb42-4da5-a873-4f5ee13de9d2
+# ╠═6a03eb1b-733c-4a9c-9ffb-73ebfbf068ac
+# ╠═2db193b4-0165-4c23-9ebe-10b8b4ea086d
+# ╠═84c5e859-54fb-422b-8c6c-71297d687c52
+# ╠═a46a2cc1-74be-4a85-a537-35afdc772aa9
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
---
+++ CNN_SurfaceCrack.jl
@@ -0,0 +1,100 @@
+### A Pluto.jl notebook ###
+# v0.19.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 5172ea02-f225-11ec-10aa-2721af1393d9
+
+
+# ╔═╡ 1997623b-948f-4fd1-b7b6-8bfefd38f042
+
+
+# ╔═╡ e4603ba9-b2f6-40ae-946b-7c49d292a040
+
+
+# ╔═╡ a266e013-eb1c-43d7-bacc-17064fb0b758
+
+
+# ╔═╡ b1082bf9-02b4-46ce-96ae-caa0ca6cffd4
+
+
+# ╔═╡ 470efa89-e261-4696-ae6d-51e554505542
+
+
+# ╔═╡ 89e97e91-d725-4f18-87a3-6f2bddcd488f
+
+
+# ╔═╡ 1e66e5b7-5bd6-4c94-a275-7a704b163e31
+
+
+# ╔═╡ ad4eb2af-0671-4d45-82c2-08b89d870d1f
+
+
+# ╔═╡ cb0fb067-38d9-4408-ac72-7c3d345b2c51
+
+
+# ╔═╡ c18d277b-7a8b-4b15-9b50-9ca40e100b1e
+
+
+# ╔═╡ 9ad44c18-ec33-470e-b4a2-803edf469792
+
+
+# ╔═╡ 81f1d01f-a29d-446b-b124-c6c992e9d465
+
+
+# ╔═╡ 7373f853-8b60-4dc3-8948-6447c52f3e9f
+
+
+# ╔═╡ 6a03eb1b-733c-4a9c-9ffb-73ebfbf068ac
+
+
+# ╔═╡ 2db193b4-0165-4c23-9ebe-10b8b4ea086d
+
+
+# ╔═╡ 84c5e859-54fb-422b-8c6c-71297d687c52
+
+
+# ╔═╡ a46a2cc1-74be-4a85-a537-35afdc772aa9
+
+
+# ╔═╡ d3d9513c-5d49-45df-9533-3c54797c979f
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═5172ea02-f225-11ec-10aa-2721af1393d9
+# ╠═1997623b-948f-4fd1-b7b6-8bfefd38f042
+# ╠═e4603ba9-b2f6-40ae-946b-7c49d292a040
+# ╠═a266e013-eb1c-43d7-bacc-17064fb0b758
+# ╠═b1082bf9-02b4-46ce-96ae-caa0ca6cffd4
+# ╠═470efa89-e261-4696-ae6d-51e554505542
+# ╠═89e97e91-d725-4f18-87a3-6f2bddcd488f
+# ╠═1e66e5b7-5bd6-4c94-a275-7a704b163e31
+# ╠═ad4eb2af-0671-4d45-82c2-08b89d870d1f
+# ╠═cb0fb067-38d9-4408-ac72-7c3d345b2c51
+# ╠═c18d277b-7a8b-4b15-9b50-9ca40e100b1e
+# ╠═9ad44c18-ec33-470e-b4a2-803edf469792
+# ╠═81f1d01f-a29d-446b-b124-c6c992e9d465
+# ╠═7373f853-8b60-4dc3-8948-6447c52f3e9f
+# ╠═6a03eb1b-733c-4a9c-9ffb-73ebfbf068ac
+# ╠═2db193b4-0165-4c23-9ebe-10b8b4ea086d
+# ╠═84c5e859-54fb-422b-8c6c-71297d687c52
+# ╠═a46a2cc1-74be-4a85-a537-35afdc772aa9
+# ╠═d3d9513c-5d49-45df-9533-3c54797c979f
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
